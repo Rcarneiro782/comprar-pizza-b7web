49bf4c702225ea2b1dfa0dfccd39fe541c0e116a
Rafael <rcarneiro782@gmail.com>
2022-03-16 11:38:02 -0300

    part3

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const c = (el) =>{
-    return document.querySelector(el);
-}
-
+const c = (el) =>document.querySelector(el);
+const cs = (el)=>document.querySelector(el);
+    
 
 pizzaJson.map((item, index)=>{
    let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
+   pizzaItem.querySelector('.pizza-item--img img').src= item.img;
 
-   c('.pizza-area').append( pizzaItem );
+
+
+
+
+
+   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)} `;
+
+
+
+   c('.pizza-area').append(pizzaItem);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
