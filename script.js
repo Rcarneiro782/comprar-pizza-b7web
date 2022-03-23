459f24221ff24972bf28feb365d3ecedc5b5edfd
Rafael <rcarneiro782@gmail.com>
2022-03-23 19:34:50 -0300

    ui

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
-const c = (el) =>document.querySelector(el);
-const cs = (el)=>document.querySelector(el);
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelector(el);
+
+
+pizzaJson.map((item, index) => {
+  let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+
+  pizzaItem.setAttribute('data-key', index);
+  pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)} `;
+  pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+  pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+  pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+  
+  pizzaItem.querySelector('a').addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let key = e.target.closest('.pizza-item').getAttribute('data-key');
+   modalQT = 1;
+   modalKey = key;
+   
+
+    c('.pizzaBig img').src = pizzaJson[key].img;
+    c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+    c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+   
+
     
 
-pizzaJson.map((item, index)=>{
-   let pizzaItem = c('.models .pizza-item').cloneNode(true);
+    cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+      
+      size.querySelector('span').innerHTML = '123' 
 
 
-   pizzaItem.setAttribute('data-key', index);
-   pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)} `;
-   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
-   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-   pizzaItem.querySelector('.pizza-item--img img').src= item.img;
-   pizzaItem.querySelector('a').addEventListener('click', (e)=>{
-     e.preventDefault();
-     let key = e.target.closest('.pizza-item').getAttribute('data-key');
-     c('.pizzaBig img').src = pizzaJson[key].img;
-     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
-     c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+    });
 
+     c('.pizzaInfo--qt').innerHTML = modalQT;
 
-
-     c('.pizzaWindowArea').style.opacity = 0;
-     c('.pizzaWindowArea').style.display = 'flex';
-     setTimeout(()=>{
+    c('.pizzaWindowArea').style.opacity = 0;
+    c('.pizzaWindowArea').style.display = 'flex';
+    setTimeout(() => {
       c('.pizzaWindowArea').style.opacity = 1;
-     }, 200);
+    }, 200);
 
 
-   });
+  });
 
 
 
-   c('.pizza-area').append(pizzaItem);
+  c('.pizza-area').append(pizzaItem);
 });
 
 
+      /*if(sizeIndex == 2) {
+        size.classList.add('selected');
+        //pizzaJson[key].sizes[sizeIndex];
+      }*/
+       //c('.pizzaInfo--size.selected').classList.remove('selected');
 
 
 
