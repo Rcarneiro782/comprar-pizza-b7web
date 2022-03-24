58c00402c59121f3f720b90357cd75b5f131c583
Rafael <rcarneiro782@gmail.com>
2022-03-24 19:30:06 -0300

    parte7

--- script.js
+++ script.js
@@ -58,6 +58,31 @@ pizzaJson.map((item, index) => {
 
   c('.pizza-area').append(pizzaItem);
 });
+// Eventos do Modal
+function closeModal(){
+  c('.pizzaWindowArea').style.opacity = 0;
+  setTimeout(()=>{
+    c('.pizzaWindowArea').style.display = 'none';
+  },500);
+
+}
+
+cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
+  item.addEventListener('click', closeModal );
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
       /*if(sizeIndex == 2) {
