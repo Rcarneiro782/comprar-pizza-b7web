1a25986f7244e329e9a4248cf6a286a8b269d0bd
Rafael <rcarneiro782@gmail.com>
2022-03-25 10:49:24 -0300

    parte8

--- script.js
+++ script.js
@@ -69,10 +69,34 @@ function closeModal(){
 
 cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
   item.addEventListener('click', closeModal );
-})
+});
 
 
 
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+  if(modalQT > 1){
+  modalQT--;
+  c('.pizzaInfo--qt').innerHTML = modalQT;
+}
+});
+
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+  modalQT++;
+  c('.pizzaInfo--qt').innerHTML = modalQT;
+});
+
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+      
+  size.addEventListener('click', (e)=>{
+    c('.pizzaInfo--size.selected').classList.remove('selected');
+    size.classList.add('selected');
+  });
+
+
+
+});
 
 
 
