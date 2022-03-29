11bd327071fb68db29e7492fbd3210b61be44879
Rafael <rcarneiro782@gmail.com>
2022-03-29 11:28:54 -0300

    erro updatecart

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let modalQT = 1;
 let cart = [];
-let modalKey= 0;
+let modalKey = 0;
 
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
@@ -15,15 +15,15 @@ pizzaJson.map((item, index) => {
   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
   pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-  
+
   pizzaItem.querySelector('a').addEventListener('click', (e) => {
     e.preventDefault();
 
     let key = e.target.closest('.pizza-item').getAttribute('data-key');
     modalQT = 1;
     modalKey = key;
-   //modalKey = key;
-   
+  
+
 
     c('.pizzaBig img').src = pizzaJson[key].img;
     c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -31,22 +31,22 @@ pizzaJson.map((item, index) => {
     c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
     c('.pizzaInfo--size.selected').classList.remove('selected');
 
-    
+
 
     cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
 
-      
-        if(sizeIndex == 2){
-          size.classList.add('selected');
-        }
-      
-        size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
-      
+
+      if (sizeIndex == 2) {
+        size.classList.add('selected');
+      }
+
+      size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex]
+
 
 
     });
 
-     c('.pizzaInfo--qt').innerHTML = modalQT;
+    c('.pizzaInfo--qt').innerHTML = modalQT;
 
     c('.pizzaWindowArea').style.opacity = 0;
     c('.pizzaWindowArea').style.display = 'flex';
@@ -62,28 +62,28 @@ pizzaJson.map((item, index) => {
   c('.pizza-area').append(pizzaItem);
 });
 // Eventos do Modal
-function closeModal(){
+function closeModal() {
   c('.pizzaWindowArea').style.opacity = 0;
-  setTimeout(()=>{
+  setTimeout(() => {
     c('.pizzaWindowArea').style.display = 'none';
-  },500);
+  }, 500);
 
 }
 
-cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
-  item.addEventListener('click', closeModal );
+cs('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item) => {
+  item.addEventListener('click', closeModal);
 });
 
 
 
-c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
-  if(modalQT > 1){
-  modalQT--;
-  c('.pizzaInfo--qt').innerHTML = modalQT;
-}
+c('.pizzaInfo--qtmenos').addEventListener('click', () => {
+  if (modalQT > 1) {
+    modalQT--;
+    c('.pizzaInfo--qt').innerHTML = modalQT;
+  }
 });
 
-c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+c('.pizzaInfo--qtmais').addEventListener('click', () => {
   modalQT++;
   c('.pizzaInfo--qt').innerHTML = modalQT;
 });
@@ -91,8 +91,8 @@ c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
 
 cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
 
-      
-  size.addEventListener('click', (e)=>{
+
+  size.addEventListener('click', (e) => {
     c('.pizzaInfo--size.selected').classList.remove('selected');
     size.classList.add('selected');
   });
@@ -101,32 +101,37 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
 
 });
 
-c('.pizzaInfo--addButton').addEventListener('click', ()=>{
- 
-let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+c('.pizzaInfo--addButton').addEventListener('click', () => {
 
-let identifier =  pizzaJson[modalKey].id+'@'+size;
+  let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
+  let identifier = pizzaJson[modalKey].id + '@' + size;
+  let key = cart.findIndex((item) => item.identifier == identifier);
 
-let key = cart.findIndex((item)=> item.identifier == identifier);
+  if (key > - 1) {
+    cart[key].qt += modalQT;
+  }
+  else {
+    cart.push({
+      identifier,
+      id: pizzaJson[modalKey].id,
+      size:size,
+      qt:modalQT
 
-if(key > - 1){
-cart[key].qt += modalQT;
-}
-else{
-cart.push({
-identifier,
-id: pizzaJson[modalKey].id,
-size:size,
-qt:modalQT
+    });
+  }
 
+  updateCart();
+  closeModal();
 });
+
+function updateCart(){
+  if (cart.lenght > 0) {
+    c('aside').classList.add('show');
+  } else {
+    c('aside').classList.remove('show');
+  }
 }
 
-closeModal();
-
-
-   
-} );
 
 
 
@@ -138,11 +143,10 @@ closeModal();
 
 
 
-
-      /*if(sizeIndex == 2) {
-        size.classList.add('selected');
-        //pizzaJson[key].sizes[sizeIndex];
-      }*/
+/*if(sizeIndex == 2) {
+  size.classList.add('selected');
+  //pizzaJson[key].sizes[sizeIndex];
+}*/
        //c('.pizzaInfo--size.selected').classList.remove('selected');
 
 
